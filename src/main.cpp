3a701940c7cfa52a4806b2327ec0151fe27b9fb5
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-04-12 23:24:31 +0530

    Bumpers added

--- src/main.cpp
+++ src/main.cpp
@@ -98,9 +98,95 @@ void keyPressed(unsigned char c, int x, int y){
     }
 }
 
+void DDA(int x0,int y0,int x1,int y1){
+    int dx=x1-x0;
+    int dy=y1-y0;
+    int steps=abs(dx)>abs(dy)?abs(dx):abs(dy);
+
+    float XInc=dx/float(steps);
+    float YInc=dy/float(steps);
+    float x=x0;
+    float y=y0;
+    glBegin(GL_POINTS);
+    for(int i=0;i<steps;i++){
+        glVertex2f(x,y);
+        x+=XInc;
+        y+=YInc;
+    }
+    glEnd();
+}
+
+void draw_pixel(int x,int y)
+{
+    glColor3f(0.5,0.4,0.7);
+    glPointSize(1.0);
+    glBegin(GL_POINTS);
+    glVertex2i(x,y);
+    glEnd();
+}
+
+void edgedetect(float x1,float y1,float x2,float y2,int *le,int *re)
+{
+    float temp,x,mx;
+    int i;
+
+    if(y1>y2)
+    {
+        temp=x1,x1=x2,x2=temp;
+        temp=y1,y1=y2,y2=temp;
+    }
+
+    if(y1==y2)
+        mx=x2-x1;
+    else
+        mx=(x2-x1)/(y2-y1);
+
+    x=x1;
+
+    for(i=int(y1);i<=(int)y2;i++)
+    {
+        if(x<(float)le[i]) le[i]=(int)x;
+        if(x>(float)re[i]) re[i]=(int)x;
+        x+=mx;
+    }
+}
+
+void scanfill(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+    int le[700],re[700],i,j;
+
+    for(i=0;i<700;i++)
+        le[i]=700,re[i]=0;
+
+    edgedetect(x1,y1,x2,y2,le,re);
+    edgedetect(x2,y2,x3,y3,le,re);
+    edgedetect(x3,y3,x1,y1,le,re);
+
+
+
+
+    for(j=0;j<700;j++)
+    {
+        if(le[j]<=re[j])
+            for(i=le[j];i<re[j];i++)
+                draw_pixel(i,j);
+    }
+}
+
 void renderScene(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // TODO: Draw all the colliding objects
+    glColor3b(1.0,1.0,0);
+    DDA(-200,-200,-450,-150);
+    DDA(-450,-150,-400,0);
+    DDA(-400,0,-200,-200);
+    scanfill(-450,-150,-400,0,-200,-200);
+
+    DDA(200,-200,450,-150);
+    DDA(450,-150,400,0);
+    DDA(400,0,200,-200);
+
+    scanfill(200,-200,400,0,450,-150);
 }
 
 void timer(int value){
