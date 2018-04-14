3ccc16f863bf01e0a1a6b4722f131fd7727c7c1a
Aayushjn <aayushjn11@gmail.com>
2018-04-14 21:35:23 +0530

    Revised code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Pinball game implementation in C++ from scratch (OpenGL)
 
 Press space to launch the ball.
-Press left arrow button to move left flipper and right arrow button for the right flipper.
+Press 'z' button to rotate the flippers.
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,16 @@
 #include <windows.h>
 #include <GL/glut.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <time.h>
+#include "functions.h"
 
-// Macros
-#define PI 3.14159625f
+GLfloat alpha = 1.0f;
 
-// Global variables
-
-GLfloat radius = 0.05f;
-GLfloat ballX = 1.8f;
-GLfloat ballY = -0.8f;
-GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
-GLfloat xSpeed = 0.0005f;
-GLfloat ySpeed = 0.0007f;
-GLdouble clipAreaLeft, clipAreaRight, clipAreaBottom, clipAreaTop;
-GLfloat alpha;
-int refreshMillis = 25;
-bool finished = false;
-
-// OpenGL functions
 void initOpenGL();
+void timer(int);
 void reshape(GLsizei, GLsizei);
 void keyPressed(unsigned char, int, int);
 void renderScene();
 
-// Helper functions
-void timer(int);
-void launchBall();
-void bounceBall();
-bool isFinished(GLfloat, GLfloat);
-
 int main(int argc, char **argv){
-    srand(time(0));
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1500, 750);
@@ -44,6 +19,7 @@ int main(int argc, char **argv){
     initOpenGL();
 
     glutDisplayFunc(renderScene);
+    //glutIdleFunc(renderScene);
     glutTimerFunc(0, timer, 0);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyPressed);
@@ -63,157 +39,30 @@ void initOpenGL(){
 }
 
 void reshape(GLsizei width, GLsizei height){
-    if(height == 0){
-        height = 1;
-    }
-    GLfloat aspect = (GLfloat)width / (GLfloat)height;
-
     glViewport(0, 0, width, height);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if(width >= height){
-        clipAreaLeft = -1.0 * aspect;
-        clipAreaRight = 1.0 * aspect;
-        clipAreaBottom = -1.0;
-        clipAreaTop = 1.0;
-    }
-    else{
-        clipAreaLeft = -1.0;
-        clipAreaRight = 1.0;
-        clipAreaBottom = -1.0 * aspect;
-        clipAreaTop = 1.0 * aspect;
-    }
-    gluOrtho2D(clipAreaLeft, clipAreaRight, clipAreaBottom, clipAreaTop);
-    ballXMin = clipAreaLeft + radius;
-    ballXMax = clipAreaRight - radius;
-    ballYMin = clipAreaBottom + radius;
-    ballYMax = clipAreaTop - radius;
 }
 
 void keyPressed(unsigned char c, int x, int y){
-    if(c == 32){
-        launchBall();
-        bounceBall();
+    if(c == 'z'){
+        rotationFlag = 1;
     }
-}
-
-void renderScene(){
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // TODO: Draw all the colliding objects
+    if(c == 32){
+        ballFlag = 1;
+    }
+    if(c == 27){
+        exit(0);
+    }
 }
 
 void timer(int value){
     glutPostRedisplay();
-    glutTimerFunc(refreshMillis, timer, 0);
+    glutTimerFunc(16, timer, 0);
 }
 
-void launchBall(){
-    alpha = 1.0f;
-    ballX = 1.8f;
-    ballY = -0.8f;
-    xSpeed = fabs(xSpeed);
-    ySpeed = fabs(ySpeed);
-    do{
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        glPushMatrix();
-        glTranslatef(ballX, ballY, 0.0f);
-        glBegin(GL_TRIANGLE_FAN);
-            glColor4f(0.0f, 0.0f, 1.0f, alpha);
-            glVertex2f(0.0f, 0.0f);
-            int segments = 100;
-            GLfloat angle;
-            for(int i = 0;i <= segments;i++){
-                angle = i * 2.0f * PI / segments;
-                glVertex2f(cos(angle) * radius, sin(angle) * radius);
-            }
-        glEnd();
-        glPopMatrix();
-
-        glutSwapBuffers();
-
-        ballY += ySpeed;
-    }while(ballY < 0.8f);
-}
-
-void bounceBall(){
-    int quadrant = (rand() % 4) + 1;
-    do{
-        finished = isFinished(ballX, ballY);
-        if(finished){
-            alpha = 0.0f;
-            ballX = 1.77f;
-            ballY = 0.77f;
-        }
-        else{
-            alpha = 1.0f;
-        }
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        glPushMatrix();
-        glTranslatef(ballX, ballY, 0.0f);
-        glBegin(GL_TRIANGLE_FAN);
-            glColor4f(0.0f, 0.0f, 1.0f, alpha);
-            glVertex2f(0.0f, 0.0f);
-            int segments = 100;
-            GLfloat angle;
-            for(int i = 0;i <= segments;i++){
-                angle = i * 2.0f * PI / segments;
-                glVertex2f(cos(angle) * radius, sin(angle) * radius);
-            }
-        glEnd();
-        glPopMatrix();
-
-        glutSwapBuffers();
-
-        switch(quadrant){
-            case 1:
-                ballX += xSpeed;
-                ballY += ySpeed;
-            break;
-            case 2:
-                ballX -= xSpeed;
-                ballY += ySpeed;
-            break;
-            case 3:
-                ballX -= xSpeed;
-                ballY -= ySpeed;
-            break;
-            case 4:
-                ballX += xSpeed;
-                ballY -= ySpeed;
-            break;
-            default:
-                printf("rand() error");
-        }
-
-        if(ballX > ballXMax){
-            ballX = ballXMax;
-            xSpeed = -xSpeed;
-        }
-        else if(ballX < ballXMin){
-            ballX = ballXMin;
-            xSpeed = -xSpeed;
-        }
-        if(ballY > ballYMax){
-            ballY = ballYMax;
-            ySpeed = -ySpeed;
-        }
-        else if(ballY < ballYMin){
-            ballY = ballYMin;
-            ySpeed = -ySpeed;
-        }
-    }while(!finished);
-}
-
-bool isFinished(GLfloat x, GLfloat y){
-    if(x >= ballXMin / 3 && x <= ballXMax / 3 && y == ballYMin){
-        return true;
-    }
-    return false;
+void renderScene(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    flipperRotation();
+    launchBall();
+    bounceBall();
+    glutSwapBuffers();
 }
